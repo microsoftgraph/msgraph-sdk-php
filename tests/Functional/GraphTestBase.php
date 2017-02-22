e28ef30cb989b2e29f2bc86bc4ab392b59df1887
Caitlin Bales (MSFT) <c.bales@outlook.com>
2017-02-21 16:42:19 -0800

    Get graph client for functional tests

--- tests/Functional/GraphTestBase.php
+++ tests/Functional/GraphTestBase.php
@@ -1,6 +1,8 @@
 <?php
 namespace Microsoft\Graph\Test;
 
+use Microsoft\Graph\Graph;
+
 class GraphTestBase
 {
 	private $clientId;
@@ -11,22 +13,45 @@ class GraphTestBase
 	private $endpoint = "https://login.microsoftonline.com/common/oauth2/token";
 	private $resource = "https%3A%2F%2Fgraph.microsoft.com%2F";
 	private $accessToken;
-	protected $graphClient;
+	public $graphClient;
 
     public function __construct()
     {
-        $clientId = TestConstants::CLIENT_ID;
-        $username = TestConstants::USERNAME;
-        $password = TestConstants::PASSWORD;
+        $this->clientId = TestConstants::CLIENT_ID;
+        $this->username = TestConstants::USERNAME;
+        $this->password = TestConstants::PASSWORD;
+
+        $this->getAuthenticatedClient();
     }
 
-    public function GetAuthenticatedClient()
+    public function getAuthenticatedClient()
     {
-
+    	if ($this->graphClient == null) 
+    	{
+    		$this->graphClient = new Graph();
+    		$this->graphClient->setAccessToken($this->getAccessToken());
+    	}
     }
 
     private function getAccessToken()
     {
+    	$body = "grant_type=".$this->grantType.
+    					 "&resource=".$this->resource.
+    					 "&client_id=".$this->clientId.
+    					 "&username=".$this->username.
+    					 "&password=".$this->password;
+    	$ch = curl_init();
+    	curl_setopt($ch, CURLOPT_URL, $this->endpoint);
+    	curl_setopt($ch, CURLOPT_POST, 1);
+    	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    	curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+		curl_setopt($ch, CURLOPT_FAILONERROR, 0);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array($this->contentType, 'Content-Length: ' . strlen($body)));
 
+		$result = curl_exec ($ch);
+		$token = json_decode($result, true)['access_token'];
+		curl_close($ch);
+
+		return $token;
     }
 }
