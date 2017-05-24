9a22e12e4dfcc4b0eef2c21fc9023c5cbf622ec4
Caitlin Bales (MSFT) <c.bales@outlook.com>
2017-05-24 09:55:09 -0700

    Add OneNote models

---
+++ src/Model/OnenoteOperation.php
@@ -0,0 +1,141 @@
+<?php
+/**
+* Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+* 
+* OnenoteOperation File
+* PHP version 7
+*
+* @category  Library
+* @package   Microsoft.Graph
+* @copyright 2016 Microsoft Corporation
+* @license   https://opensource.org/licenses/MIT MIT License
+* @version   GIT: 0.1.0
+* @link      https://graph.microsoft.io/
+*/
+namespace Microsoft\Graph\Model;
+
+/**
+* OnenoteOperation class
+*
+* @category  Model
+* @package   Microsoft.Graph
+* @copyright 2016 Microsoft Corporation
+* @license   https://opensource.org/licenses/MIT MIT License
+* @version   Release: 0.1.0
+* @link      https://graph.microsoft.io/
+*/
+class OnenoteOperation extends Operation
+{
+    /**
+    * Gets the resourceLocation
+    *
+    * @return string The resourceLocation
+    */
+    public function getResourceLocation()
+    {
+        if (array_key_exists("resourceLocation", $this->_propDict)) {
+            return $this->_propDict["resourceLocation"];
+        } else {
+            return null;
+        }
+    }
+    
+    /**
+    * Sets the resourceLocation
+    *
+    * @param string $val The resourceLocation
+    *
+    * @return OnenoteOperation
+    */
+    public function setResourceLocation($val)
+    {
+        $this->_propDict["resourceLocation"] = $val;
+        return $this;
+    }
+    
+    /**
+    * Gets the resourceId
+    *
+    * @return string The resourceId
+    */
+    public function getResourceId()
+    {
+        if (array_key_exists("resourceId", $this->_propDict)) {
+            return $this->_propDict["resourceId"];
+        } else {
+            return null;
+        }
+    }
+    
+    /**
+    * Sets the resourceId
+    *
+    * @param string $val The resourceId
+    *
+    * @return OnenoteOperation
+    */
+    public function setResourceId($val)
+    {
+        $this->_propDict["resourceId"] = $val;
+        return $this;
+    }
+    
+    /**
+    * Gets the error
+    *
+    * @return OnenoteOperationError The error
+    */
+    public function getError()
+    {
+        if (array_key_exists("error", $this->_propDict)) {
+            if (is_a($this->_propDict["error"], "Microsoft\Graph\Model\OnenoteOperationError")) {
+                return $this->_propDict["error"];
+            } else {
+                $this->_propDict["error"] = new OnenoteOperationError($this->_propDict["error"]);
+                return $this->_propDict["error"];
+            }
+        }
+        return null;
+    }
+    
+    /**
+    * Sets the error
+    *
+    * @param OnenoteOperationError $val The error
+    *
+    * @return OnenoteOperation
+    */
+    public function setError($val)
+    {
+        $this->_propDict["error"] = $val;
+        return $this;
+    }
+    
+    /**
+    * Gets the percentComplete
+    *
+    * @return string The percentComplete
+    */
+    public function getPercentComplete()
+    {
+        if (array_key_exists("percentComplete", $this->_propDict)) {
+            return $this->_propDict["percentComplete"];
+        } else {
+            return null;
+        }
+    }
+    
+    /**
+    * Sets the percentComplete
+    *
+    * @param string $val The percentComplete
+    *
+    * @return OnenoteOperation
+    */
+    public function setPercentComplete($val)
+    {
+        $this->_propDict["percentComplete"] = $val;
+        return $this;
+    }
+    
+}
