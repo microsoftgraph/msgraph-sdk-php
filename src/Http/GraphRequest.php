8db31ef438e2b18f8728c4dfefee1122d215af60
Zombaya <Zombaya@users.noreply.github.com>
2018-08-03 11:34:09 +0200

    Fix for #91

--- src/Http/GraphRequest.php
+++ src/Http/GraphRequest.php
@@ -415,6 +415,11 @@ class GraphRequest
     */
     private function _getRequestUrl()
     {
+        //Send request with opaque URL
+        if (stripos($this->endpoint, "http") === 0) {
+            return $this->endpoint;
+        }
+
         return $this->apiVersion . $this->endpoint;
     }
 
