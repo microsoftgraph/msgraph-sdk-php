e3e97e8572b5936af7626e5bf886b39ed9914f46
Michael Mainer <mmainer@microsoft.com>
2018-10-26 15:13:29 -0700

    Added the ItemPreviewInfo type for use by DriveItem

--- src/Core/GraphConstants.php
+++ src/Core/GraphConstants.php
@@ -9,7 +9,7 @@
 *
 * @category  Library
 * @package   Microsoft.Graph
-* @copyright 2016 Microsoft Corporation
+* @copyright 2018 Microsoft Corporation
 * @license   https://opensource.org/licenses/MIT MIT License
 * @version   GIT: 1.0.0
 * @link      https://graph.microsoft.io/
@@ -24,7 +24,7 @@ final class GraphConstants
     const REST_ENDPOINT = "https://graph.microsoft.com/";
 
     // Define HTTP request constants
-    const SDK_VERSION = "1.4.0";
+    const SDK_VERSION = "1.5.0";
 
     // Define error constants
     const MAX_PAGE_SIZE = 999;
