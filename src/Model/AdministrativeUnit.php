570f806d1a36c42be89ae719d484e2bf812c1a7d
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2020-09-15 13:11:34 +0000

    Update generated files with build 3677498

--- src/Model/AdministrativeUnit.php
+++ src/Model/AdministrativeUnit.php
@@ -24,4 +24,169 @@ namespace Microsoft\Graph\Model;
 */
 class AdministrativeUnit extends DirectoryObject
 {
+    /**
+    * Gets the description
+    *
+    * @return string The description
+    */
+    public function getDescription()
+    {
+        if (array_key_exists("description", $this->_propDict)) {
+            return $this->_propDict["description"];
+        } else {
+            return null;
+        }
+    }
+    
+    /**
+    * Sets the description
+    *
+    * @param string $val The description
+    *
+    * @return AdministrativeUnit
+    */
+    public function setDescription($val)
+    {
+        $this->_propDict["description"] = $val;
+        return $this;
+    }
+    
+    /**
+    * Gets the displayName
+    *
+    * @return string The displayName
+    */
+    public function getDisplayName()
+    {
+        if (array_key_exists("displayName", $this->_propDict)) {
+            return $this->_propDict["displayName"];
+        } else {
+            return null;
+        }
+    }
+    
+    /**
+    * Sets the displayName
+    *
+    * @param string $val The displayName
+    *
+    * @return AdministrativeUnit
+    */
+    public function setDisplayName($val)
+    {
+        $this->_propDict["displayName"] = $val;
+        return $this;
+    }
+    
+    /**
+    * Gets the visibility
+    *
+    * @return string The visibility
+    */
+    public function getVisibility()
+    {
+        if (array_key_exists("visibility", $this->_propDict)) {
+            return $this->_propDict["visibility"];
+        } else {
+            return null;
+        }
+    }
+    
+    /**
+    * Sets the visibility
+    *
+    * @param string $val The visibility
+    *
+    * @return AdministrativeUnit
+    */
+    public function setVisibility($val)
+    {
+        $this->_propDict["visibility"] = $val;
+        return $this;
+    }
+    
+
+     /** 
+     * Gets the members
+     *
+     * @return array The members
+     */
+    public function getMembers()
+    {
+        if (array_key_exists("members", $this->_propDict)) {
+           return $this->_propDict["members"];
+        } else {
+            return null;
+        }
+    }
+    
+    /** 
+    * Sets the members
+    *
+    * @param DirectoryObject $val The members
+    *
+    * @return AdministrativeUnit
+    */
+    public function setMembers($val)
+    {
+		$this->_propDict["members"] = $val;
+        return $this;
+    }
+    
+
+     /** 
+     * Gets the scopedRoleMembers
+     *
+     * @return array The scopedRoleMembers
+     */
+    public function getScopedRoleMembers()
+    {
+        if (array_key_exists("scopedRoleMembers", $this->_propDict)) {
+           return $this->_propDict["scopedRoleMembers"];
+        } else {
+            return null;
+        }
+    }
+    
+    /** 
+    * Sets the scopedRoleMembers
+    *
+    * @param ScopedRoleMembership $val The scopedRoleMembers
+    *
+    * @return AdministrativeUnit
+    */
+    public function setScopedRoleMembers($val)
+    {
+		$this->_propDict["scopedRoleMembers"] = $val;
+        return $this;
+    }
+    
+
+     /** 
+     * Gets the extensions
+     *
+     * @return array The extensions
+     */
+    public function getExtensions()
+    {
+        if (array_key_exists("extensions", $this->_propDict)) {
+           return $this->_propDict["extensions"];
+        } else {
+            return null;
+        }
+    }
+    
+    /** 
+    * Sets the extensions
+    *
+    * @param Extension $val The extensions
+    *
+    * @return AdministrativeUnit
+    */
+    public function setExtensions($val)
+    {
+		$this->_propDict["extensions"] = $val;
+        return $this;
+    }
+    
 }
