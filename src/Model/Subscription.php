6f0a3e30133898e887b5de63aeebf3ebb024ec25
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2020-10-20 13:11:04 +0000

    Update generated files with build 3902079

--- src/Model/Subscription.php
+++ src/Model/Subscription.php
@@ -262,6 +262,7 @@ class Subscription extends Entity
     
     /**
     * Gets the latestSupportedTlsVersion
+    * Specifies the latest version of Transport Layer Security (TLS) that the notification endpoint, specified by notificationUrl, supports. The possible values are: v1_0, v1_1, v1_2, v1_3. For subscribers whose notification endpoint supports a version lower than the currently recommended version (TLS 1.2), specifying this property by a set timeline allows them to temporarily use their deprecated version of TLS before completing their upgrade to TLS 1.2. For these subscribers, not setting this property per the timeline would result in subscription operations failing. For subscribers whose notification endpoint already supports TLS 1.2, setting this property is optional. In such cases, Microsoft Graph defaults the property to v1_2.
     *
     * @return string The latestSupportedTlsVersion
     */
@@ -276,6 +277,7 @@ class Subscription extends Entity
     
     /**
     * Sets the latestSupportedTlsVersion
+    * Specifies the latest version of Transport Layer Security (TLS) that the notification endpoint, specified by notificationUrl, supports. The possible values are: v1_0, v1_1, v1_2, v1_3. For subscribers whose notification endpoint supports a version lower than the currently recommended version (TLS 1.2), specifying this property by a set timeline allows them to temporarily use their deprecated version of TLS before completing their upgrade to TLS 1.2. For these subscribers, not setting this property per the timeline would result in subscription operations failing. For subscribers whose notification endpoint already supports TLS 1.2, setting this property is optional. In such cases, Microsoft Graph defaults the property to v1_2.
     *
     * @param string $val The latestSupportedTlsVersion
     *
