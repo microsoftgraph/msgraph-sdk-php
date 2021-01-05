80570deb805cc398986c49cf332e1c16e5c5b501
Michael Mainer <MIchaelMainer@users.noreply.github.com>
2021-01-05 10:55:01 -0800

    Update generated files with build 35879 (#367)

--- src/Model/DefaultUserRolePermissions.php
+++ src/Model/DefaultUserRolePermissions.php
@@ -7,7 +7,7 @@
 *
 * @category  Library
 * @package   Microsoft.Graph
-* @copyright © Microsoft Corporation. All rights reserved.
+* @copyright (c) Microsoft Corporation. All rights reserved.
 * @license   https://opensource.org/licenses/MIT MIT License
 * @link      https://graph.microsoft.com
 */
@@ -17,7 +17,7 @@ namespace Microsoft\Graph\Model;
 *
 * @category  Model
 * @package   Microsoft.Graph
-* @copyright © Microsoft Corporation. All rights reserved.
+* @copyright (c) Microsoft Corporation. All rights reserved.
 * @license   https://opensource.org/licenses/MIT MIT License
 * @link      https://graph.microsoft.com
 */
