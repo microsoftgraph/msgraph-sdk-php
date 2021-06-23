2047082d42f098a9457001ca812cdd2d71edc684
Alex <1797626+alex-code@users.noreply.github.com>
2021-06-23 10:50:29 +0100

    key exists check on getODataType
    Addition of key exists check on `getODataType` as it might not exist.
    #538

--- src/Model/Entity.php
+++ src/Model/Entity.php
@@ -87,11 +87,15 @@ class Entity implements \JsonSerializable
     /**
     * Gets the ODataType
     *
-    * @return string The ODataType
+    * @return string|null The ODataType
     */
     public function getODataType()
     {
-        return $this->_propDict["@odata.type"];
+        if (array_key_exists("@odata.type", $this->_propDict)) {
+            return $this->_propDict["@odata.type"];
+        } else {
+            return null;
+        }
     }
     
     /**
