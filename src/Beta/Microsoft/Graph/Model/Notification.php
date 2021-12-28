68c9c85713cefa234e23f282bde69f4bed862ce1
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2021-12-28 10:17:09 +0000

    Update generated files with build 62261

--- src/Beta/Microsoft/Graph/Model/Notification.php
+++ src/Beta/Microsoft/Graph/Model/Notification.php
@@ -26,7 +26,7 @@ class Notification extends Entity
 {
     /**
     * Gets the displayTimeToLive
-    * Sets how long (in seconds) this notification content will stay in each platform’s notification viewer. For example, when the notification is delivered to a Windows device, the value of this property is passed on to ToastNotification.ExpirationTime, which determines how long the toast notification will stay in the user’s Windows Action Center.
+    * Sets how long (in seconds) this notification content will stay in each platform's notification viewer. For example, when the notification is delivered to a Windows device, the value of this property is passed on to ToastNotification.ExpirationTime, which determines how long the toast notification will stay in the user's Windows Action Center.
     *
     * @return int|null The displayTimeToLive
     */
@@ -41,7 +41,7 @@ class Notification extends Entity
 
     /**
     * Sets the displayTimeToLive
-    * Sets how long (in seconds) this notification content will stay in each platform’s notification viewer. For example, when the notification is delivered to a Windows device, the value of this property is passed on to ToastNotification.ExpirationTime, which determines how long the toast notification will stay in the user’s Windows Action Center.
+    * Sets how long (in seconds) this notification content will stay in each platform's notification viewer. For example, when the notification is delivered to a Windows device, the value of this property is passed on to ToastNotification.ExpirationTime, which determines how long the toast notification will stay in the user's Windows Action Center.
     *
     * @param int $val The displayTimeToLive
     *
