00b1416331259bcb06c53b3bb2230a24b30c3486
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2022-01-18 10:08:59 +0000

    Bump up minor version

--- src/Core/GraphConstants.php
+++ src/Core/GraphConstants.php
@@ -23,7 +23,7 @@ final class GraphConstants
     const REST_ENDPOINT = "https://graph.microsoft.com/";
 
     // Define HTTP request constants
-    const SDK_VERSION = "1.49.0";
+    const SDK_VERSION = "1.50.0";
 
     // Define error constants
     const MAX_PAGE_SIZE = 999;
