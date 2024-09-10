ecada4b9abd7288d263301aadd63bf05ebc06f44
Microsoft Graph DevX Tooling <GraphTooling@service.microsoft.com>
2024-09-10 10:42:54 +0000

    feat(generation): update request builders and models
    Update generated files with build 162820

--- src/Generated/Sites/SitesRequestBuilderGetQueryParameters.php
+++ src/Generated/Sites/SitesRequestBuilderGetQueryParameters.php
@@ -5,7 +5,7 @@ namespace Microsoft\Graph\Generated\Sites;
 use Microsoft\Kiota\Abstractions\QueryParameter;
 
 /**
- * List all available sites in an organization. Specific filter criteria and query options are also supported and described below: In addition, you can use a $search query against the /sites collection to find sites matching given keywords.If you want to list all sites across all geographies, refer to getAllSites. For more guidance about building applications that use site discovery for scanning purposes, see Best practices for discovering files and detecting changes at scale.
+ * Search across a SharePoint tenant for sites that match keywords provided. The only property that works for sorting is createdDateTime. The search filter is a free text search that uses multiple properties when retrieving the search results.
 */
 class SitesRequestBuilderGetQueryParameters 
 {
